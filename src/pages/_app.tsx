a636052bd29ebe8db4354bcf2b4e59fdd51d1522
Bill Cui <bill.cui.work@gmail.com>
2023-01-29 12:46:31 -0500

    add ddog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { datadogRum } from "@datadog/browser-rum";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,24 @@ const theme = createTheme({
     },
   },
 });
+
+datadogRum.init({
+  applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID!,
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN!,
+  site: "datadoghq.com",
+  service: "tripplanner",
+  env: process.env.NEXT_PUBLIC_DATADOG_ENV!,
+  // Specify a version number to identify the deployed version of your application in Datadog
+  // version: '1.0.0',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: "mask-user-input",
+});
+
+datadogRum.startSessionReplayRecording();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
